55ed6f48e32082b529c4dd32dc4c8b09d2862edd
Tanner Williams <tannerwill756@gmail.com>
2020-05-20 15:55:35 -0400

    repair method working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,1 +1,15 @@
 const { succeed, fail, repair, get } = require("./enhancer");
+
+let item = {
+  name: "Tanner",
+  enhancement: 5,
+  durability: 15,
+};
+
+describe("method testing", () => {
+  describe("repair()", () => {
+    it("should restore the durability back to 100", () => {
+      expect(repair(item).durability).toBe(100);
+    });
+  });
+});
