9c8e60ad3da1ae9b379a9dd9e2f34205283fdd30
Tanner Williams <tannerwill756@gmail.com>
2020-05-20 16:55:09 -0400

    succeed and fail methods

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,33 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (typeof item.enhancement === "string") {
+    throw "enhancement must be an integer between 0 and 20";
+  } else if (item.enhancement < 0) {
+    return { ...item, enhancement: 0 };
+  } else if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else if (item.enhancement > 20) {
+    return { ...item, enhancement: 20 };
+  } else if (item.enhancement === 20) {
+    return { ...item, enhancement: item.enhancement };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement > 15) {
+    if (item.enhancement > 16) {
+      return {
+        ...item,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1,
+      };
+    } else {
+      return { ...item, durability: item.durability - 10 };
+    }
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,14 +2,24 @@ const { succeed, fail, repair, get } = require("./enhancer");
 
 let item = {
   name: "Tanner",
-  enhancement: 5,
-  durability: 15,
+  enhancement: 18,
+  durability: 18,
 };
 
 describe("method testing", () => {
   describe("repair()", () => {
     it("should restore the durability back to 100", () => {
       expect(repair(item).durability).toBe(100);
+    });
+  });
+  describe("succeed()", () => {
+    it("should increase enhancement by 1 if its not at 20", () => {
+      expect(succeed(item).enhancement).toBe(19);
+    });
+  });
+  describe("fail()", () => {
+    it("if enhancement is more than 15 decrease durability by 10 but if the enhancement is more than 16 decrease durability by 10 and decrease enhancement by 1. If enhancement is less than 15 decrease durability by 5.", () => {
+      expect(fail(item).enhancement).toBe(17);
     });
   });
 });
